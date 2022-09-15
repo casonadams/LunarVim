4034e6b858f33698b433761bd7ad2c5e516eb80e
Cason Adams <casonadams@gmail.com>
2022-09-15 00:58:32 -0600

    add: lastplace

--- lua/lvim/plugins.lua
+++ lua/lvim/plugins.lua
@@ -25,6 +25,17 @@ local core_plugins = {
       })
     end,
   },
+  {
+    "ethanholz/nvim-lastplace",
+    event = "BufRead",
+    config = function()
+      require("nvim-lastplace").setup({
+        lastplace_ignore_buftype = { "quickfix", "nofile", "help" },
+        lastplace_ignore_filetype = { "gitcommit", "gitrebase", "svn", "hgcommit" },
+        lastplace_open_folds = true,
+      })
+    end,
+  },
   { "Tastyep/structlog.nvim" },
 
   { "nvim-lua/popup.nvim" },
